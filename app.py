425112f05350380e7b1c501b0ebcbb984efdb264
Mashrafi Mohammad Monon <mashrafi@Mashrafis-MacBook-Air.local>
2023-03-20 17:49:07 +0600

    initial xl testing

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ import openpyxl as xl
 wb = xl.load_workbook("Python Tutorial Supplementary Materials/transactions.xlsx")
 sheet = wb['Sheet1']
 print(sheet['a1'].value)
+
+for row in range(2, sheet.max_row + 1):
+    cell = sheet.cell(row, 3)
+    print(cell.value)
