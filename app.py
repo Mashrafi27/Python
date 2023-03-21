301f2f0cecde0e17b60d7aae347e33bbda333c5a
Mashrafi Mohammad Monon <mashrafi@Mashrafis-MacBook-Air.local>
2023-03-21 10:11:41 +0600

    initial workbook task

--- app.py
+++ app.py
@@ -6,3 +6,8 @@ print(sheet['a1'].value)
 for row in range(2, sheet.max_row + 1):
     cell = sheet.cell(row, 3)
     print(cell.value)
+    corrected_price = cell.value*0.9
+    corrected_price_cell = sheet.cell(row,4)
+    corrected_price_cell.value = corrected_price
+
+wb.save('transactions2.xlsx')
